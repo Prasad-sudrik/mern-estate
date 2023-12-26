15d0d5e74b79d1b30999fd57e4ed8cd819d18ab8
Prasad Sudrik <sudrikprasad90@gmail.com>
2023-12-26 09:16:49 +0530

    add create listing api route

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,9 +14,11 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOutUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
-
 export default function Profile() {
   const fileRef = useRef(null);
   const dispatch = useDispatch();
@@ -98,6 +100,20 @@ export default function Profile() {
       dispatch(updateUserFailure(error.message));
     }
   };
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch(`/api/auth/signout`);
+      const data = await res.json();
+      if (data.success == false) {
+        dispatch(signOutUserFailure(data));
+        return;
+      }
+      dispatch(signOutUserSuccess(data));
+    } catch (error) {
+      dispatch(signOutUserFailure(error.message));
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -166,7 +182,9 @@ export default function Profile() {
         >
           Delete Account
         </span>
-        <span className="text-red-700 cursor-pointer">Sign Out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
+          Sign Out
+        </span>
       </div>
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
       <p className="text-green-700 mt-5">
