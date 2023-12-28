804c3b9d0f532bf1ed6823493aa0e22a66ac8be3
Prasad Sudrik <sudrikprasad90@gmail.com>
2023-12-28 21:45:56 +0530

    error removed

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -27,7 +27,7 @@ export default function ListingItem({ listing }) {
           <p className="text-slate-500 mt-2 font-semibold">
             $
             {listing.offer
-              ? listing.discountedPrice.toLocaleString("en-US")
+              ? listing.discountPrice.toLocaleString("en-US")
               : listing.regularPrice.toLocaleString("en-US")}
             {listing.type === "rent" && " / month"}
           </p>
